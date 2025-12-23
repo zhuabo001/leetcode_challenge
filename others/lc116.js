2219ed05523b6aae9ecc5aae51034b1bc760b3fb
iFlow CLI <you@example.com>
2025-12-24 00:37:06 +0800

    添加 lc116.js 算法实现

--- others/lc116.js
+++ others/lc116.js
@@ -1,16 +1,23 @@
-// 填充每个节点的下一个右侧节点指针
-//  给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+// 116. 填充每个节点的下一个右侧节点指针
+// 给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+// struct Node {
+//   int val;
+//   Node *left;
+//   Node *right;
+//   Node *next;
+// }
+// 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+// 初始状态下，所有 next 指针都被设置为 NULL。
 function Node(val, left, right, next) {
   this.val = val === undefined ? null : val;
   this.left = left === undefined ? null : left;
   this.right = right === undefined ? null : right;
   this.next = next === undefined ? null : next;
 }
-//  填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
-//  初始状态下，所有 next 指针都被设置为 NULL。
+
 const connect = (root) => {
-  const queue = [];
-  if (root) queue.push(root);
+  if (!root) return null;
+  const queue = [root];
   while (queue.length) {
     const len = queue.length;
     for (let i = 0; i < len; i++) {
