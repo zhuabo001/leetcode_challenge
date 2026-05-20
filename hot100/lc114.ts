78f9a35670e34688b4b4a1182dcd124a2e97eaa1
iFlow CLI <you@example.com>
2026-05-21 00:24:47 +0800

    feat: 添加二叉树展开为链表等解法

---
+++ hot100/lc114.ts
@@ -0,0 +1,20 @@
+// 二叉树展开为链表
+import { TreeNode } from './type';
+const flatten = (root: TreeNode) => {
+  if (!root) return;
+  flatten(root.left as TreeNode);
+  flatten(root.right as TreeNode);
+  const left = root.left;
+  const right = root.right;
+  root.right = left;
+  root.left = null;
+  let pointer = root;
+  while (pointer.right) {
+    pointer = pointer.right;
+  }
+  pointer.right = right;
+  return root;
+};
+// 时间复杂度 O(n^2) ——> 递归O(n)嵌套了一个while找最右端的循环
+
+// 空间复杂度 O(h) h是树高
---
+++ hot100/lc21.ts
@@ -0,0 +1,18 @@
+// 合并两个有序链表。
+import { ListNode } from './type';
+const mergedList = (list_1: ListNode, list_2: ListNode) => {
+  const preHead = { value: -1, next: null } as ListNode;
+  let cur = preHead;
+  while (list_1 && list_2) {
+    if (list_1 < list_2) {
+      cur.next = list_1;
+      list_1 = list_1.next as ListNode;
+    } else {
+      cur.next = list_2;
+      list_2 = list_2.next as ListNode;
+    }
+    cur = cur.next;
+  }
+  cur.next = list_1 ? list_1 : list_2;
+  return preHead.next;
+};
