3f7dde760a179b34feb32faa6d0f3a200205efb1
iFlow CLI <you@example.com>
2026-06-09 00:08:00 +0800

    feat: 新增搜索二维矩阵、字母异位词分组题解

---
+++ hot100/lc49.ts
@@ -0,0 +1,23 @@
+// 字母异位词分组
+const groupAnagrams = (strs: string[]): unknown[][] => {
+  if (!strs || strs.length === 0) return [[]];
+  const ans = new Map();
+  for (const str of strs) {
+    const tempStr = str
+      .split('')
+      .sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0))
+      .join('');
+    if (ans.has(tempStr)) {
+      ans.get(tempStr).push(str);
+    } else {
+      ans.set(tempStr, [str]);
+    }
+  }
+  return Array.from(ans.values());
+};
+
+const example = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'];
+console.log(groupAnagrams(example));
+
+// 时间复杂度 O(N + klogk) - k是每个小字符串的长度
+// 空间复杂度 O(N * k) 存储n个字符串每个字符串长度为k
